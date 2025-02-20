6bfed0eb155ed5590ac5370a738f142c07041649
whoatebambi <142728697+whoatebambi@users.noreply.github.com>
2025-02-20 17:00:46 +0100

    pollfd structure initialized

--- Server.cpp
+++ Server.cpp
@@ -6,18 +6,11 @@
 /*   By: florencecousergue <florencecousergue@st    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 12:29:01 by fcouserg          #+#    #+#             */
-/*   Updated: 2025/02/20 13:19:06 by florencecou      ###   ########.fr       */
+/*   Updated: 2025/02/20 16:53:03 by florencecou      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Server.hpp"
-
-#include <iostream>
-#include <cstring>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h>
-#include <fcntl.h>
 
 // bool Server::Signal = false; //-> initialize the static boolean
 
@@ -47,9 +40,8 @@ void Server::CloseFds() {
 // 3️⃣ Set non-blocking mode → fcntl()
 // 4️⃣ Bind the socket to specified address/port → bind()
 // 5️⃣ Mark the socket for listening → listen()
-void Server::start() {
-	_port = 4444;
-    _socketFd = socket(AF_INET, SOCK_STREAM, 0); // socket() creates the server socket
+void Server::InitSocket() {
+	_socketFd = socket(AF_INET, SOCK_STREAM, 0); // socket() creates the server socket
     if (_socketFd == -1)
         throw(std::runtime_error("Socket creation failed"));
 	
@@ -59,14 +51,14 @@ void Server::start() {
     serverAddr.sin_port = htons(_port); // Converts the port to network byte order (big endian)
     serverAddr.sin_addr.s_addr = INADDR_ANY; // Means “listen on all available network interfaces.”
 
-	// std::cout << "\n─── server\n";
-    // std::cout << "\t├── _port\t" << _port << "\n";
-    // std::cout << "\t└── _socketFd\t" << _socketFd << "\n";
-    // std::cout << "\n─── serverAddr\n";
-    // std::cout << "\t├── sin_family\t" << "AF_INET" << serverAddr.sin_family << "\n";
-    // std::cout << "\t├── sin_port\t" << ntohs(serverAddr.sin_port) << "\n";
-    // std::cout << "\t└── sin_addr\t" << "\n";
-    // std::cout << "\t    └── s_addr\t" << serverAddr.sin_addr.s_addr << "\n\n";
+	std::cout << "\n─── server\n";
+    std::cout << "\t├── _port\t" << _port << "\n";
+    std::cout << "\t└── _socketFd\t" << _socketFd << "\n";
+    std::cout << "\n─── serverAddr\n";
+    std::cout << "\t├── sin_family\t" << "AF_INET" << serverAddr.sin_family << "\n";
+    std::cout << "\t├── sin_port\t" << ntohs(serverAddr.sin_port) << "\n";
+    std::cout << "\t└── sin_addr\t" << "\n";
+    std::cout << "\t    └── s_addr\t" << serverAddr.sin_addr.s_addr << "\n";
 
 	int en = 1;
 	// By default, when a server closes a socket, the operating system keeps the port blocked for a short period 
@@ -88,19 +80,41 @@ void Server::start() {
 
 	// Associates _socketFd with a specific IP address and port stored in serverAddr.
 	// This allows clients to know where to connect.
-    if (bind(_socketFd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == -1) {
+    if (bind(_socketFd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == -1)
+		throw(std::runtime_error("Bind failed"));
 		// Reasons for failure: port already in use, socket not created properly,
 		//  address not available (e.g. requires admin privileges).
-		throw(std::runtime_error("Bind failed"));
-    }
 
 	// Tells the OS that this socket is now ready to accept 128 connections
 	// (SOMAXCONN is the max amount allowed per kernel).
-    if (listen(_socketFd, SOMAXCONN) == -1) {
-		// Reasons for failure: socket not properly bound, system is out of resources.
+    if (listen(_socketFd, SOMAXCONN) == -1)
 		throw(std::runtime_error("Listen failed"));
-    }
+		// Reasons for failure: socket not properly bound, system is out of resources.
+}
 
-    std::cout << "Server listening on port " << _port << "\n";
+void Server::InitPollFd() {
+	pollfd NewPoll;
+
+	NewPoll.fd = this->_socketFd; //-> add the server socket to the pollfd
+	NewPoll.events = POLLIN; //-> set the event to POLLIN for reading data
+	NewPoll.revents = 0; //-> set the revents to 0
+	fds.push_back(NewPoll); //-> add the server socket to the pollfd
+
+	std::cout << "\n─── NewPoll\n";
+    std::cout << "\t├── fd\t\t" << NewPoll.fd << "\n"; //-> file descriptor
+    std::cout << "\t├── events\t" << NewPoll.events << "(POLLIN)" << "\n"; //-> requested events
+    std::cout << "\t└── revents\t" << NewPoll.revents << "\n\n"; //-> returned events
+
+}
+
+void Server::start() {
+	this->_port = 4444;
+    
+	InitSocket();
+	InitPollFd();
+
+	std::cout << "Server <" << this->_socketFd << "> Connected" << std::endl;
+	std::cout << "Waiting to accept a connection...\n";
+    // std::cout << "Server listening on port " << _port << "\n";
     while (true);
 }
--- Server.hpp
+++ Server.hpp
@@ -6,24 +6,35 @@
 /*   By: florencecousergue <florencecousergue@st    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 12:29:01 by fcouserg          #+#    #+#             */
-/*   Updated: 2025/02/20 12:17:38 by florencecou      ###   ########.fr       */
+/*   Updated: 2025/02/20 16:26:15 by florencecou      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
+#include <iostream>
+#include <cstring>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <poll.h>
+#include <vector>
+
 class Server {
+	private:
+		int _port;
+		int _socketFd;
+		// static bool Signal; //-> static boolean for signal
+		std::vector<struct pollfd> fds; //-> vector of pollfd
+		
 	public:
 		Server();
 		~Server();
 		void start();
 
-		void CloseFds();
-
+		void	CloseFds();
+		void	InitSocket();
+		void	InitPollFd();
 		// static void SignalHandler(int signum); //-> signal handler
-	
-	private:
-		int _port;
-		int _socketFd;
-		// static bool Signal; //-> static boolean for signal
 };
