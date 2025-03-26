2689c959fc9160b076bde511aa75cb7c24f68d70
ft-jules <jpointil@42.student.fr>
2025-03-26 13:36:22 +0100

    removed chesnay irc

--- chesnayIRC/src/PingCommand.cpp
+++
@@ -1,9 +0,0 @@
-#include "PingCommand.hpp"
-
-
-void PingCommand::execute(const std::string &args, CliSocket *client)
-{
-    std::string trimmed = (args);
-
-    sendMsg(client, "PONG " + trimmed);
-}
